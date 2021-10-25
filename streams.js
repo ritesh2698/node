53b3cc8f69b70fd8d7e9e4c50d37bc7f1a37ffbc
Ritesh <riteshprajapati30@gmail.com>
2021-10-25 18:56:02 +0530

    initial

--- streams.js
+++ streams.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
 
 const readStream = fs.createReadStream('./text3.txt',{encoding: 'utf-8'})
+const writestream = fs.createReadStream('./text4.txt')
+
 
 readStream.on('data',(chunk) => {
     console.log("----New Chunk----")
     console.log(chunk)
+
+writestream.write('\New Chunk')
+writestream.write(chunk);
 })
